da7a6aa14c2acd1e45a74d46ba170d464646d9c2
sebastian argaez <sargaezu@gmail.com>
2019-08-03 16:43:21 -0500

    Crud de Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,23 +1,52 @@
 class CategoriesController < ApplicationController
+before_action :set_category, only: %i[show update edit destroy]
+
+  def index
+    @categories = Category.all
+  end
   
   def new
       @category = Category.new
   end
-  def create
-      @category = Category.new(category_params)
 
+  def create
+      
       if @category.save
-        # redirect_to expenses_path
+        redirect_to categories_path
         flash[:notice] = "Categoria creada satisfactoriamente"        
       else
         render :new
       end  
   end
 
+  def edit
+    
+  end
+
+  def update
+    
+      if @category.update(category_params)
+        redirect_to categories_path, notice: "La Categoría ha sido Editada"
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: "La Categoría ha sido Eliminada"    
+  end
+
+
+
   private
 
   def category_params
     params.require(:category).permit(:name)
   end  
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
 end
