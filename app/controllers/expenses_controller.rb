e8b31cac54af0e4611d3571371da34d8233bfc4e
sebastian argaez <sargaezu@gmail.com>
2019-08-10 16:54:42 -0500

    Migraciones-correcctión amount

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -8,4 +8,10 @@ class ExpensesController < ApplicationController
     @expense = Expense.new
   end
 
+
+  private
+
+  def params_expenses
+    params.require(:expenses).permit(:id, :type_id, :date, :concept, :category_id, :amount).merge(user:current_user)  
+  end
 end
