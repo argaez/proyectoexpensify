1e7de35d24af68237668198add3012dfd08110f4
sebastian argaez <sargaezu@gmail.com>
2019-08-10 17:36:48 -0500

    Listar expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,7 @@
 class ExpensesController < ApplicationController
   def index
     @tab = :expenses
+    @expenses = Expense.all
     @categories = Category.all
   end
 
@@ -8,10 +9,20 @@ class ExpensesController < ApplicationController
     @expense = Expense.new
   end
 
+  def create
+    @expense = Expense.new(params_expenses)
+    if @expense.save
+      redirect_to expenses_path
+      flash[:notice] = "Expense ha sido creado satisfactoriamente"   
+    else
+      render :new
+    end
+
+  end
 
   private
 
   def params_expenses
-    params.require(:expenses).permit(:id, :type_id, :date, :concept, :category_id, :amount).merge(user:current_user)  
+    params.require(:expense).permit(:id, :type_id, :date, :concept, :category_id, :amount).merge(user:current_user)  
   end
 end
